936e36e1468c06bc0052e71fd10046b6c8e37bb7
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-05 20:27:20 +0300

    Monday 1 commit

--- src/Components/Dislpay.tsx
+++ src/Components/Dislpay.tsx
@@ -6,11 +6,11 @@ type DisplayType={
 }
 export const Display=({error,count,...props}:DisplayType)=>{
     return(
-        // <div className={error?'error':'second'}>{count}</div>
+        <div className={error?'error':'second'}>{count}</div>
 
-    <div className={error?'error':'second'}>
-        {!error?count
-        :<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnzJKFQE5O_VBRCwfqzl5JyN0F5XybL0sCCw&usqp=CAU" alt=""/>}
-    </div>
+    // <div className={error?'error':'second'}>
+    //     {!error?count
+    //     :<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnzJKFQE5O_VBRCwfqzl5JyN0F5XybL0sCCw&usqp=CAU" alt=""/>}
+    // </div>
     )
 }
