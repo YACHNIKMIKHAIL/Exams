f2b33e17ae129c9f45536c7f603a8751291382db
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-09 17:05:36 +0300

    1 commit disabled( buttons continue

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -9,15 +9,19 @@ type ButtonType = {
     maxValue: number
 }
 export const Button = ({callback, name, ...props}: ButtonType) => {
+    const isInc = name === 'Inc'
+    const isRes = name === 'Reset'
+    const isGet = name === 'Get'
+
+    const IncButtom = (props.value === props.maxValue || (props.value < 0 || props.value > props.maxValue))
+        ? ''
+        : 'disabled'
+    const ResButtom = (props.minValue === 0) ? 'disabled' : ''
+    const Buttom = (isInc ? IncButtom : isGet) ? '' : isRes ? ResButtom : 'disabled'
+
 
     return (
-        <button className={
-            (props.value === props.minValue && name === 'Inc' || props.value === props.maxValue && name === 'Inc'
-                || props.value === props.minValue && name === 'Reset'
-                || props.value !== 0 && name === 'Get'
-
-            ) ? 'disabled' : ''
-        }
+        <button className={Buttom}
                 onClick={(e) => callback()}>{name}</button>
     )
 }
